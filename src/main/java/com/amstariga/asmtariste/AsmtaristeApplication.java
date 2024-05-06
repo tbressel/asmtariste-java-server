34d558d8bc5223f88f46caa0e3d51a428ca9927f
tbressel <tbressel.dev@gmail.com>
2024-05-06 17:39:31 +0200

    core : Spring Boot Project first install

---
+++ src/main/java/com/amstariga/asmtariste/AsmtaristeApplication.java
@@ -0,0 +1,13 @@
+package com.amstariga.asmtariste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmtaristeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmtaristeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amstariga/asmtariste/AsmtaristeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amstariga.asmtariste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmtaristeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
